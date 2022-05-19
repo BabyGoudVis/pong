4b35c6a8f3b78b4e91e1c3e45e329b2d42b0a583
BabyGoudVis <cottynjurre@gmail.com>
2022-05-19 18:05:55 +0200

    Created extra classes

--- main.py
+++ main.py
@@ -6,58 +6,57 @@ from random import *
 from math import cos, radians, sin
 from time import sleep
 
-FPS = 60
-WIDTH, HEIGHT = 1280, 720
-
-
 WHITE = (255, 255, 255)
 BLACK = (25, 25, 25)
-VELOCITY = 600  # pixels / s
-BASE_BALL_VELOCITY = 300  # pixels / s
 
-pygame.init()
-pygame.display.set_caption("pong pong pong!")
-surface = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
 
-baseFont = pygame.font.SysFont("Arial", int(HEIGHT / 10))
+class Game:
+    FPS = 60
+    WIDTH = 1280
+    HEIGHT = 720
 
-playButton = pygame.Rect((WIDTH * 0.76875)/2 - 20, HEIGHT / 2 - 20,
-                         WIDTH * 0.23125 + 40, int(HEIGHT / 10) + 40)
+    @staticmethod
+    def get_fps():
+        return Game.FPS
 
-filename = 'data.json'
-with open(filename, 'r') as f:
-    data = json.load(f)
-    highScore = data['highscore']
-    newHighscore = data['newHighscore']
+    @staticmethod
+    def get_width():
+        return Game.WIDTH
+
+    @staticmethod
+    def get_height():
+        return Game.HEIGHT
 
 
 class Platform:
     def __init__(self):
-        self.width = WIDTH / 4
-        self.height = HEIGHT / 70
-        self.x = (WIDTH - self.width) / 2
-        self.y = (HEIGHT / 5) * 4
-        self.velocity = VELOCITY / FPS
-        self.platform = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.WIDTH = Game.WIDTH / 4
+        self.HEIGHT = Game.HEIGHT / 70
+        self.x = (Game.WIDTH - self.WIDTH) / 2
+        self.y = (Game.HEIGHT / 5) * 4
+        self.velocity = 600 / Game.FPS
+        self.platform = pygame.Rect(
+            self.x, self.y, self.WIDTH, self.HEIGHT)
 
     def checkPosition(self):
         if platform.platform.x < 0:
             platform.platform.x = 0
-        if platform.platform.x + platform.width > WIDTH:
-            platform.platform.x = WIDTH - platform.width
+        if platform.platform.x + platform.WIDTH > Game.WIDTH:
+            platform.platform.x = Game.WIDTH - platform.WIDTH
 
     def draw(self):
         pygame.draw.rect(surface, WHITE, self.platform)
 
 
 class Ball:
+    BASE_BALL_VELOCITY = 300
+
     def __init__(self):
         self.radius = 10
-        self.x = WIDTH / 2
-        self.y = HEIGHT / 5
+        self.x = Game.WIDTH / 2
+        self.y = Game.HEIGHT / 5
         self.angle = randint(0, 180)
-        self.BaseVelocity = BASE_BALL_VELOCITY / FPS
+        self.BaseVelocity = Ball.BASE_BALL_VELOCITY / Game.FPS
         self.xVelocity = 0
         self.yVelocity = 0
         self.ball = pygame.Rect(self.x, self.y, self.radius, self.radius)
@@ -71,11 +70,33 @@ class Ball:
             ball.angle -= 360
         if ball.angle < 0:
             ball.angle += 360
-        ball.BaseVelocity = (BASE_BALL_VELOCITY /
-                             FPS) ** (1 + (counter // 5) / 5)
+        ball.BaseVelocity = (Ball.BASE_BALL_VELOCITY /
+                             Game.FPS) ** (1 + (counter // 5) / 5)
 
     def draw(self):
         pygame.draw.rect(surface, WHITE, self.ball)
+
+
+platform = Platform()
+ball = Ball()
+game = Game()
+
+
+pygame.init()
+pygame.display.set_caption("pong pong pong!")
+surface = pygame.display.set_mode((Game.WIDTH, Game.HEIGHT))
+clock = pygame.time.Clock()
+
+baseFont = pygame.font.SysFont("Arial", int(Game.HEIGHT / 10))
+
+playButton = pygame.Rect((Game.WIDTH * 0.76875)/2 - 20, Game.HEIGHT / 2 - 20,
+                         Game.WIDTH * 0.23125 + 40, int(Game.HEIGHT / 10) + 40)
+
+filename = 'data.json'
+with open(filename, 'r') as f:
+    data = json.load(f)
+    highScore = data['highscore']
+    newHighscore = data['newHighscore']
 
 
 def drawing():
@@ -84,36 +105,37 @@ def drawing():
         case "home":
             text_surface = baseFont.render(
                 f"High Score: {highScore}", True, WHITE)
-            surface.blit(text_surface, (WIDTH / 100, HEIGHT / 100))
+            surface.blit(text_surface, (Game.WIDTH / 100, Game.HEIGHT / 100))
             text_surface = baseFont.render("New Game", True, WHITE)
-            surface.blit(text_surface, ((WIDTH * 0.76875)/2, HEIGHT / 2))
+            surface.blit(
+                text_surface, ((Game.WIDTH * 0.76875)/2, Game.HEIGHT / 2))
             pygame.draw.rect(surface, WHITE, playButton, 2)
         case "game":
             text_surface = baseFont.render(
                 f"Score: {scoreMessage}", True, WHITE)
-            surface.blit(text_surface, (WIDTH / 100, HEIGHT / 100))
+            surface.blit(text_surface, (Game.WIDTH / 100, Game.HEIGHT / 100))
             platform.draw()
             ball.draw()
         case "game over" | "waiting":
             text_surface = baseFont.render("Game over!", True, WHITE)
-            surface.blit(text_surface, (WIDTH / 5, HEIGHT // 4))
+            surface.blit(text_surface, (Game.WIDTH / 5, Game.HEIGHT // 4))
             if newHighscore:
                 text_surface = baseFont.render(
                     f"Score: {counter}     New High Score!!", True, WHITE)
             else:
                 text_surface = baseFont.render(
                     f"Score: {counter}     High Score: {highScore}", True, WHITE)
-            surface.blit(text_surface, (WIDTH / 5, HEIGHT // 3))
+            surface.blit(text_surface, (Game.WIDTH / 5, Game.HEIGHT // 3))
             text_surface = baseFont.render(
                 f"Press space to go to home screen", True, WHITE)
-            surface.blit(text_surface, (WIDTH / 5, HEIGHT // 2))
+            surface.blit(text_surface, (Game.WIDTH / 5, Game.HEIGHT // 2))
 
     pygame.display.update()
 
 
 def getCollition(platform, ball, counter):
     collisionTolerance = ball.BaseVelocity * 1.1
-    if ball.ball.left <= 0 or ball.ball.right >= WIDTH:
+    if ball.ball.left <= 0 or ball.ball.right >= Game.WIDTH:
         ball.angle = 180 - ball.angle - randint(-5, 5)
     if ball.ball.top <= 0:
         ball.angle *= -1
@@ -126,7 +148,7 @@ def getCollition(platform, ball, counter):
             ball.angle = 180 - ball.angle
         if abs(platform.platform.left - ball.ball.right) <= collisionTolerance:
             ball.angle = 180 - ball.angle
-    if ball.ball.bottom >= HEIGHT:
+    if ball.ball.bottom >= Game.HEIGHT:
         return counter, "game over"
     return counter, "game"
 
@@ -146,7 +168,7 @@ def handleMovementPlatform():
 
     if pygame.mouse.get_pressed()[0]:
         xMouse, yMouse = pygame.mouse.get_pos()
-        platform.platform.x = xMouse - platform.width / 2
+        platform.platform.x = xMouse - platform.WIDTH / 2
 
 
 def gameOver():
@@ -155,8 +177,6 @@ def gameOver():
     return False
 
 
-platform = Platform()
-ball = Ball()
 counter = 0
 
 status = "home"
@@ -203,4 +223,4 @@ while True:
                 status = "home"
     # print(f"Status: {status}")
     drawing()
-    clock.tick(FPS)
+    clock.tick(Game.FPS)
